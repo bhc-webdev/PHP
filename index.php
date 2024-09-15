ceb837bcc4337402a7bacb035fac92a9cd09d69c
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-09-14 19:22:45 -0500

    index update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$name = "Anthony Reyna"
+$name = "Anthony Reyna";
 echo $name . "'s PHP Web Page";
 
 echo "Hello, and welcom to my page.";
@@ -9,13 +9,13 @@ I like palying video games, plauing D&D, and watching videos on the interent.";
 echo "I am taking this class to learn more programming lanugages, and to complete my Web Design degree.";
 
 $myString = "I know the moon, and this is an alien city - Amy Lowell, A London Throughfare. 2 A.M.";
-echo $myString
+echo $myString;
 
-$brookhavenName = "Dallas College Brookhaven Campus"
-$brookhavenStreet = "3939 Valley View Lane"
-$brookhavenCity = "Farmers Branch"
-$brookhavenState = "Texas"
-$brookhavenPostal = "75244"
+$brookhavenName = "Dallas College Brookhaven Campus";
+$brookhavenStreet = "3939 Valley View Lane";
+$brookhavenCity = "Farmers Branch";
+$brookhavenState = "Texas";
+$brookhavenPostal = "75244";
 
 $brookhavenAddress = $brookhavenName . $brookhavenStreet . $brookhavenCity . $brookhavenState . $brookhavenPostal;
 
@@ -30,7 +30,7 @@ $product = $x * $y;
 $quotient = $x / $y;
 $modulus = $x % $y;
 
-echo $x . " + " . $y . " = " . $sum
+echo $x . " + " . $y . " = " . $sum;
 echo $x . " - " . $y . " = " . $difference;
 echo $x . " * " . $y . " = " . $product;
 echo $x . " / " . $y . " = " . $quotient;
@@ -42,7 +42,5 @@ echo $currentPage;
 //This is a comment for the assignment.
 //This is the first version of the file.
 //I will add more comments to show more updates.
-
-
 
 ?>
