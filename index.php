bd26882662660acc7e221976faa56e9e17e39a0c
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-09-21 12:44:43 -0500

    index update 7.4

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
 <?php
 
 $name = "Anthony Reyna";
-echo "<p>" . $name . "'s PHP Web Page" . "</p>";
+echo $name . "'s PHP Web Page";
+echo "<br>";
 
-echo "<p>Hello, and welcome to my page.</p>";
-echo "<p>My name is Anthony Reyna. I am 21 years old, and I am currently a college student.</p>"
-"<p>I like palying video games, plauing D&D, and watching videos on the interent.</p>";
-echo "<p>I am taking this class to learn more programming lanugages, and to complete my Web Design degree.</p>";
+echo "Hello, and welcome to my page.";
+echo "<br>";
+echo "My name is Anthony Reyna. I am 21 years old, and I am currently a college student.<br>"
+"I like palying video games, plauing D&D, and watching videos on the interent.";
+echo "<br>";
+echo "I am taking this class to learn more programming lanugages, and to complete my Web Design degree.";
+echo "<br>";
 
-$myString = "<p>I know the moon, and this is an alien city - Amy Lowell, A London Throughfare. 2 A.M.</p>";
+$myString = "I know the moon, and this is an alien city - Amy Lowell, A London Throughfare. 2 A.M.";
 echo $myString;
+echo "<br>";
 
-$brookhavenName = "Dallas College Brookhaven Campus ";
-$brookhavenStreet = "3939 Valley View Lane ";
-$brookhavenCity = "Farmers Branch ";
-$brookhavenState = "Texas ";
-$brookhavenPostal = "75244 ";
+$brookhavenName = "Dallas College Brookhaven Campus";
+$brookhavenStreet = " 3939 Valley View Lane";
+$brookhavenCity = " Farmers Branch";
+$brookhavenState = " Texas";
+$brookhavenPostal = " 75244";
 
 $brookhavenAddress = $brookhavenName . $brookhavenStreet . $brookhavenCity . $brookhavenState . $brookhavenPostal;
 
-echo "<p>" . $brookhavenAddress . "</p>";
+echo $brookhavenAddress;
+echo "<br>";
 
 $x = 37;
 $y = 6;
@@ -30,19 +36,24 @@ $product = $x * $y;
 $quotient = $x / $y;
 $modulus = $x % $y;
 
-echo "<p>" . $x . " + " . $y . " = " . $sum . "</p>";
-echo "<p>" . $x . " - " . $y . " = " . $difference . "/<p>";
-echo "<p>" . $x . " * " . $y . " = " . $product . "/<p>";
-echo "<p>" . $x . " / " . $y . " = " . $quotient . "</p>";
-echo "<p>" . $x . " % " . $y . " = " . $modulus . "</p>";
+echo $x . " + " . $y . " = " . $sum;
+echo "<br>";
+echo $x . " - " . $y . " = " . $difference;
+echo "<br>";
+echo $x . " * " . $y . " = " . $product;
+echo "<br>";
+echo $x . " / " . $y . " = " . $quotient;
+echo "<br>";
+echo $x . " % " . $y . " = " . $modulus;
+echo "<br>";
 
 $currentPage= $_SERVER['SCRIPT_NAME'];
-echo "<p>" . $currentPage . "</p>";
+echo $currentPage;
 
 //This is a comment for the assignment.
 //This is the first version of the file.
 //I will add more comments to show more updates.
 //Update One: Forgot to add semicolons to certain lines, and added them where needed.
 //Update Two: Added line breaks to sperate everything.
-//Update: Testing paragraph tags.
+
 ?>
