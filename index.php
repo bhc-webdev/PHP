cc7797657d1a3fffcfd6f216ad8ff58ef38f4681
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-09-21 13:23:31 -0500

    index br tag 2

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
 $name = "Anthony Reyna";
-echo $name . "'s PHP Web Page<br>";
+echo $name . "'s PHP Web Page" . "<br>";
 
-echo "Hello, and welcome to my page.<br>";
-echo "My name is Anthony Reyna. I am 21 years old, and I am currently a college student.<br>"
-"I like palying video games, plauing D&D, and watching videos on the interent.<br>";
-echo "I am taking this class to learn more programming lanugages, and to complete my Web Design degree.<br>";
+echo "Hello, and welcome to my page." . "<br>";
+echo "My name is Anthony Reyna. I am 21 years old, and I am currently a college student." . "<br>"
+"I like palying video games, plauing D&D, and watching videos on the interent." . "<br>";
+echo "I am taking this class to learn more programming lanugages, and to complete my Web Design degree." . "<br>";
 
 $myString = "I know the moon, and this is an alien city - Amy Lowell, A London Throughfare. 2 A.M.";
 echo $myString . "<br>";
