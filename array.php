67a5760454535ddfbaebdc4ac66d41af64e3bfef
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-09-26 21:55:06 -0500

    array reset test

--- array.php
+++ array.php
@@ -1,22 +1,37 @@
 <?php
+$pageContent = "<h1>Favorite Albums</h1>";
 
 $albumRatings = ["Mama's Gun" => "9", "Introspection" => "8", "Stereotype A" => "10", "Marble" => "7", "This is the One" => "8"];
 $albumRatings["Abbey Road"] = "10";
-ksort($albumRatings);
+  
+ksort($albumRatings)
+foreach ($albumRatings as $albumTilte => $rating) {
+    echo "\n<p>$albumTitle is a $rating out of 10.</p>";
+}
 
-$pageContent = "<h1>Favorite Albums</h1>" . "<p>$value</p>";
-
-$band = array (
-    "The Beatles" = array("A Hard Day's Night,"=>"1964","Help!,"=>"1965","Rubber Soul,"=>"1965","Abbey Road,"=>"1969");
-    "Led Zepplin" = array("Led Zepplin IV,"=>"1971");
-    "Rolling Stones" = array("Let It Bleed,"=>"1969","Sticky Fingers,"=>"1971");
-    "The Who" = array("Tommy"=>"1969","Quadrophenia"=>"1973","The Who by Numbers"=>"1975");
-);
-
-$value = $band ["The Who"] ["1969"];
+$beatles = ["A Hard Day's Night"=>"1964", "Help!"=>"1965", "Rubber Soul"=>"1965", "Abbey Road"=>"1969"];
+$ledZepplin = ["Led Zepplin IV"=>"1971"]; 
+$rollingStones = ["Let It Bleed"=>"1969", "Sticky Fingers"=>"1971"];
+$theWho = ["Tommy"=>"1969", "Quadrophenia"=>"1973", "The Who by Numbers"=>"1975"];
 
 
-/*
+$band = ["The Beatles" => $beatles, "Led Zepplin" => $ledZepplin, "Rolling Stones" => $rollingStones, "The Who" => $theWho];
+
+foreach ($band as $bandName => $albumList) {
+    $pageContent . "\n<h2>$band</h2><ul>";
+    foreach ($albumList as $songTitle => $songYear) {
+        echo "\n<li>$songTitle - $songYear</li>";
+    }
+    echo "\n</ul>";
+}
+
+
+
+
+
+/*$value = $band ["The Who"] ["1969"];
+. "$albumRatings" . "<p>$value</p>"
+
 foreach ($band as $subArray => $band) {
     $pageContent . "\n<p>The band " . $subArray . " released:<br>";
     foreach ($band as $albumName => $albumYear) {
@@ -51,6 +66,11 @@ foreach ($band70s as $subArray => $band70s) {
     }
 
     echo "</p>";
+
+
+
+ksort($movies); 37 echo '<tr><td colspan="2"><strong> Sorted by title:</strong></td></tr>'; 38 foreach ($movies as $title => $rating) { 39 echo "<tr><td>$rating</td> 40 <td>$title</td></tr>\n"; 41 }
+
 }
     */
 
